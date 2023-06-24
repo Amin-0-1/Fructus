b1d7bed4c4e07407acdeafa33426037a5cc29d36
Amin <mahmoudamin373@gmail.com>
2023-06-24 03:33:11 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Amin on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
